d85d68cff79453f77c22910e3d3da06101e9bf20
Jetrca92 <blaz.guzej@gmail.com>
2024-05-09 21:01:43 +0200

    Setup vue

---
+++ wawtonet/src/components/NavBar.vue
@@ -0,0 +1,58 @@
+<template>
+
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+
+        <div class="navbar-brand">
+            <a class="navbar-item" href="">
+                <img alt="wawtonet logo" src="../assets/wawtonetlogo.png">
+                <span class="ml-1">WAWTONET</span>
+            </a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" @click="toggleMobileMenu">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu" :class="{'is-active': showMobileMenu}">
+            
+            <div class="navbar-start">
+                <a class="navbar-item">
+                    Avto oglasi
+                </a>
+                <a class="navbar-item">
+                    Zadnjih 100
+                </a>
+                <a class="navbar-item">
+                    Objavi oglas
+                </a>
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-primary">
+                            <strong>Sign up</strong>
+                        </a>
+                        <a class="button is-light">
+                            Log in
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </nav>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const showMobileMenu = ref(false)
+const toggleMobileMenu = () => {
+  showMobileMenu.value = !showMobileMenu.value
+}
+</script>
